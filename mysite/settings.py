224e8690eedfcc2e30134aec222a3da1085ba17d
HayashiYoshihiro <y.hayashi.24.4.1400@4nonome.com>
2024-05-04 16:42:09 +0900

    カスタムユーザーモデルの実装

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "welcome.apps.WelcomeConfig",
 ]
 
+AUTH_USER_MODEL = "accounts.User"
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
